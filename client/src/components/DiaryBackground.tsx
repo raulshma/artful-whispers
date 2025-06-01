063cf4234aaa7a1f0bcd31570dd175cdde7ff549
raulshma <raulshma@gmail.com>
2025-06-01 22:57:11 +0530

    imp: transition time

--- client/src/components/DiaryBackground.tsx
+++ client/src/components/DiaryBackground.tsx
@@ -17,7 +17,7 @@ export default function DiaryBackground({ imageUrl }: DiaryBackgroundProps) {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 1.5 }}
         />
       )}
     </AnimatePresence>
